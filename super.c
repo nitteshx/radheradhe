4ca8f14d9385e2c27e7695a1caf77bad753f21c7
nitteshx <niteshkumar03961@gmail.com>
2026-09-19 17:11:19 +0530

    our 3nd class of os

---
+++ super.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double first, second, result;
+    char operator;
+
+    printf("Enter an expression (e.g. 12.5 * 4): ");
+    if (scanf("%lf %c %lf", &first, &operator, &second) != 3) {
+        fprintf(stderr, "Invalid input.\n");
+        return 1;
+    }
+
+    switch (operator) {
+    case '+':
+        result = first + second;
+        break;
+    case '-':
+        result = first - second;
+        break;
+    case '*':
+        result = first * second;
+        break;
+    case '/':
+        if (second == 0.0) {
+            fprintf(stderr, "Error: division by zero.\n");
+            return 1;
+        }
+        result = first / second;
+        break;
+    default:
+        fprintf(stderr, "Invalid operator. Use +, -, *, or /.\n");
+        return 1;
+    }
+
+    printf("Result: %g\n", result);
+    return 0;
+}
